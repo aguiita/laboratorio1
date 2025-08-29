d79074938926de00c35352d614d80caf1c98180b
aguiita <145377675+aguiita@users.noreply.github.com>
2025-08-29 17:31:27 -0300

    Create prueba5.py

---
+++ prueba5.py
@@ -0,0 +1,21 @@
+
+  File c:\users\agmendez\prueba2\moneda3d\prueba3.py:45
+    print(f"Imagen {archivo} leída con forma {img.shape} y tipo {img.dtype}")
+
+NameError: name 'img' is not defined
+
+
+runcell(1, 'C:/Users/agmendez/prueba2/moneda3d/prueba3.py')
+Cantidad de imágenes encontradas: 26
+Primer archivo: C:/Users/agmendez/prueba2/moneda3d\prueba-004-20250708-164956.tif
+No se pudo leer C:/Users/agmendez/prueba2/moneda3d\prueba-004-20250708-164956.tif: corrupted tag list at offset 283810
+Traceback (most recent call last):
+
+  File C:\ProgramData\anaconda3\Lib\site-packages\spyder_kernels\py3compat.py:356 in compat_exec
+    exec(code, globals, locals)
+
+  File c:\users\agmendez\prueba2\moneda3d\prueba3.py:45
+    print(f"Imagen {archivo} leída con forma {img.shape} y tipo {img.dtype}")
+
+NameError: name 'img' is not defined
+
